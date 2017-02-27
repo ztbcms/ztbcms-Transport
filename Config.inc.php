f014f15d4991873a1ad08fc52c6cc39912f7fd12
Jayin Ton <tonjayin@gmail.com>
2017-02-28 00:34:51 +0800

    typo

--- Config.inc.php
+++ Config.inc.php
@@ -21,7 +21,7 @@ return array(
 	'authoremail' => 'tonjayin@gmail.com',
 	//版本号，请不要带除数字外的其他字符
 	'version' => '1.0.1.0',
-	//适配最低ZtbFCMS版本，
+	//适配最低ZtbCMS版本，
 	'adaptation' => '2.0.0.0',
 	//签名
 	'sign' => '2fa528afab8210bbcaf5ce73e03a0555',
